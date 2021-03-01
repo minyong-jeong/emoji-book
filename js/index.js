6818febe123cd8a2f2c44cd8d2c1efd7b8b8a741
Minyong Jeong <jmy3155@gmail.com>
2021-03-01 18:24:26 +0900

    Change emoji data variable

--- js/index.js
+++ js/index.js
@@ -1,97 +1,4 @@
-const all = smileys.concat(
-    handGestures,
-    people,
-    clothingAndAccessories,
-    animals,
-    nature,
-    foodsAndDrink,
-    activity,
-    travelAndPlaces,
-    objects,
-    symbols,
-    flags
-);
-
-let preNav = "all";
 let currentNav = "all";
-
-function createPage(nav) {
-    const cardContainer = document.querySelector(".card-container");
-    cardContainer.innerHTML = "";
-
-    let items;
-    preNav = currentNav;
-    switch (nav) {
-        case "all":
-            items = all;
-            currentNav = "all";
-            break;
-        case "smileys":
-            items = smileys;
-            currentNav = "smileys";
-            break;
-        case "handGestures":
-            items = handGestures;
-            currentNav = "handGestures";
-            break;
-        case "people":
-            items = people;
-            currentNav = "people";
-            break;
-        case "clothingAndAccessories":
-            items = clothingAndAccessories;
-            currentNav = "clothingAndAccessories";
-            break;
-        case "animals":
-            items = animals;
-            currentNav = "animals";
-            break;
-        case "nature":
-            items = nature;
-            currentNav = "nature";
-            break;
-        case "foodsAndDrink":
-            items = foodsAndDrink;
-            currentNav = "foodsAndDrink";
-            break;
-        case "activity":
-            items = activity;
-            currentNav = "activity";
-            break;
-        case "travelAndPlaces":
-            items = travelAndPlaces;
-            currentNav = "travelAndPlaces";
-            break;
-        case "objects":
-            items = objects;
-            currentNav = "objects";
-            break;
-        case "symbols":
-            items = symbols;
-            currentNav = "symbols";
-            break;
-        case "flags":
-            items = flags;
-            currentNav = "flags";
-            break;
-        default:
-            console.log(`Cannot found nav (${nav})`);
-            return;
-    }
-
-    document.getElementById(preNav).style.backgroundColor = 'rgb(35, 130, 255)';
-    document.getElementById(currentNav).style.backgroundColor = 'rgb(255, 61, 61)';
-    for (var i = 0; i < items.length; i++) {
-        const div = document.createElement("div");
-        div.className = "card";
-        div.innerHTML = `
-        <div class="card-text">${items[i]["emoji"]}</div>
-        <div class="card-description" hidden>${items[i]["description"]}</div>
-        <div class="darkness" onclick="copyStringToClipboard('${items[i]["emoji"]}')">COPY</div>
-        `
-        cardContainer.appendChild(div);
-    }
-}
 
 function copyStringToClipboard (str) {
     console.log(str + " copied!");
@@ -106,14 +13,23 @@ function copyStringToClipboard (str) {
     notification(str);
 }
 
-function notification(str) {
-    const noti = document.createElement('div');
-    noti.className = "noti";
-    noti.innerText = str + " Copied!";
-    document.body.appendChild(noti);
-    setTimeout(() => {
-        document.body.removeChild(noti);
-    }, 1000);
+function createPage(nav) {
+    const items = data[nav];
+    const cardContainer = document.querySelector(".card-container");
+    cardContainer.innerHTML = "";
+    document.getElementById(currentNav).style.backgroundColor = 'rgb(35, 130, 255)';
+    document.getElementById(nav).style.backgroundColor = 'rgb(255, 61, 61)';
+    for (var i = 0; i < items.length; i++) {
+        const div = document.createElement("div");
+        div.className = "card";
+        div.innerHTML = `
+        <div class="card-text">${items[i]["emoji"]}</div>
+        <div class="card-description" hidden>${items[i]["description"]}</div>
+        <div class="darkness" onclick="copyStringToClipboard('${items[i]["emoji"]}')">COPY</div>
+        `
+        cardContainer.appendChild(div);
+    }
+    currentNav = nav;
 }
 
 function initNav() {
@@ -123,6 +39,16 @@ function initNav() {
             createPage(tabs[i].id);
         });
     }
+}
+
+function notification(str) {
+    const noti = document.createElement('div');
+    noti.className = "noti";
+    noti.innerText = str + " Copied!";
+    document.body.appendChild(noti);
+    setTimeout(() => {
+        document.body.removeChild(noti);
+    }, 1000);
 }
 
 function tableFilter() {
@@ -140,6 +66,20 @@ let filter, i, txtValue;
 }
 
 window.onload = () => {
+    data["all"] = [
+        ...data["smileys"],
+        ...data["handGestures"],
+        ...data["people"],
+        ...data["clothingAndAccessories"],
+        ...data["animals"],
+        ...data["nature"],
+        ...data["foodsAndDrink"],
+        ...data["activity"],
+        ...data["travelAndPlaces"],
+        ...data["objects"],
+        ...data["symbols"],
+        ...data["flags"]
+    ];
     createPage("all");
     initNav();
     document.getElementById("myinput").addEventListener("keyup", tableFilter);
